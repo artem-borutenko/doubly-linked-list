25187fbcbb22ca01253e743684d73c36fe2c1fec
Artem Borutenko <artem.borutenko@gmail.com>
2017-01-29 17:55:53 +0300

    full

--- src/linked-list.js
+++ src/linked-list.js
@@ -7,63 +7,45 @@ class LinkedList {
         this._tail = null;
         this.length=0;
 	}
-   
-	
-	
-	
+ 
     append(data) {
 		
 		var node =new Node(data);
 		if(this.length==0)
 			this._head=node;
-		  
-		
 
-		else{
+		else
+		{
 			this._tail.next=node;
 		    node.prev=this._tail;
-			
-			
+
 		}
-		this._tail=node;
-	
-	    this.length++;
-		
-		
-		
+			this._tail=node;
+			this.length++;
+			return this;
+
 	}
 
     head() {
-		
-		
+
 			return this._head.data;
-		
-		
-		
 	}
 
     tail() {
-		
+
 			return this._tail.data;
-		
-		
-		
-		
 	}
 
     at(index) {
 		
 		var current=this._head;
-		for(var i=0;i<index;i++){
-			
+		for(var i=0;i<index;i++)
+		{
 			current=current.next;
-			
-			
+
 		}
 		return current.data;
-		
-		
-		
+
 	}
 
     insertAt(index, data) {
@@ -76,29 +58,23 @@ class LinkedList {
 			
 		}
 		current.data=data;
-		
-		
-		
-		
+		return this;
 		
 	}
 
     isEmpty() {
+		
 		return this.length==0;
-		
-		
+
 	}
 
     clear() {
-	
-	
+
 		this._head=this._tail=new Node();
 		
 		this.length=0;
-	
-		
-			
-			
+		return this;
+
 		}
 
 	
@@ -118,27 +94,48 @@ class LinkedList {
                 
 
             }
-            else
+          else
             {
                 this._head = current.next;
               
             }
-            if (current.next != null)
+          if (current.next != null)
             {
                 current.next.prev = current.prev;
 
             }
-            else
+          else
             {
                 this._tail = current.prev;
             }
+			
+		this.length--;
+		return this;
 
 	}
 
-    reverse() {}
+    reverse() {
+		
+		    var current = this._tail;
+			var temp;
+
+        while(current!=null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+
+            current =temp;
+        }
+
+			temp = this._head;
+			this._head = this._tail;
+			this._tail = temp;    
+
+    
+			return this;
+	}
 
     indexOf(data) {
-	
 		
 		var current=this._head;
 		var index=0;
